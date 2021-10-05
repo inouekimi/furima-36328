8c1132936ec4ec0789887d5af186ab961843df13
inouekimi <kool.wxyz@gmail.com>
2021-10-06 04:04:21 +0900

    商品削除機能の編集

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root to: "items#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :items
-  resources :items, only: [:new, :create, :show, :edit, :update]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
 end
